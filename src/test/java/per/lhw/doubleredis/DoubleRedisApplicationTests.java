d51108824e12147be812b9c4e687f6c14371feed
lhwarthas <liuliu139@126.com>
2017-08-15 18:50:56 +0800

    Copy Test

--- src/test/java/per/lhw/doubleredis/DoubleRedisApplicationTests.java
+++ src/test/java/per/lhw/doubleredis/DoubleRedisApplicationTests.java
@@ -2,15 +2,31 @@ package per.lhw.doubleredis;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import per.lhw.doubleredis.redis.PrimaryRedisService;
+import per.lhw.doubleredis.redis.SecondaryRedisService;
+
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DoubleRedisApplicationTests {
 
+    @Autowired
+    private PrimaryRedisService primaryRedisService;
+
+    @Autowired
+    private SecondaryRedisService secondaryRedisService;
+
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void primaryToSecondary() throws Exception {
+        Map<String, String> map = primaryRedisService.multiGet("");
+        secondaryRedisService.multiSet(map);
+    }
 }
